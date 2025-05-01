64ce1f7cf50aaba81f1da64114e6c83050e8cca7
jose123457 <jronisgp@gmail.com>
2025-05-01 16:05:12 -0400

    Added login image with modal and improved mobile responsiveness

--- app.py
+++ app.py
@@ -10,4 +10,8 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    # Configuración para desarrollo local
+    # app.run(debug=True, host='127.0.0.1', port=8000)
+    
+    # Configuración para producción en Render
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000))) 
