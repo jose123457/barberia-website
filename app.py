9f706ac45f695814a4a5fcb62ebc4071337e0586
jose123457 <jronisgp@gmail.com>
2025-05-01 15:20:16 -0400

    Fix deployment configuration

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__, static_folder='barberia_scraper/static', template_folder='barberia_scraper/templates')
+app = Flask(__name__, 
+            static_folder='barberia_scraper/static', 
+            template_folder='barberia_scraper/templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000))) 
